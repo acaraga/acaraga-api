a9f505e555e5f20497ff5231d9cb4037da002655
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-12-14 16:40:34 +0700

    feat: create token

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,17 @@ import { eventsRoute } from "./modules/event/routes";
 import { categoriesRoute } from "./modules/category/routes";
 import { cors } from "hono/cors";
 
+import { userRoute } from "./modules/user/route";
+import { authRoute } from "./modules/auth/route";
+
 const app = new OpenAPIHono();
 
 app.use(cors());
 
 app.route("/events", eventsRoute);
 app.route("/categories", categoriesRoute);
+app.route("/users", userRoute);
+app.route("/auth", authRoute);
 
 app.doc("/openapi.json", {
   openapi: "3.0.0",
